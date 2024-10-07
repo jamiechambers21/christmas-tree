be0d80b6a3e3d8649075dd44b730e5c5a400ddb8
jamiechambers21 <59603715+jamiechambers21@users.noreply.github.com>
2024-10-07 19:26:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,8 @@
-import logging
-import logging.handlers
-import os
+from datetime import datetime
 
-import requests
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
-
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
-
+def update_log():
+    with open('log.txt', 'a') as f:
+        f.write(f'Automated commit at {datetime.now()}\n')
 
 if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
-
-    r = requests.get('https://weather.talkpython.fm/api/weather?city=Madrid&country=ES')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Madrid: {temperature}')
+    update_log()
